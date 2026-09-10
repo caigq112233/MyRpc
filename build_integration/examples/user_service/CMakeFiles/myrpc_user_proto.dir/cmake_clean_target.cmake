9b30fe87567f3fe0dc0a85712b5de1affbd5e18f
CaiGuangQing <13052739120@163.com>
2026-09-10 21:07:17 +0800

    V2

--- build_integration/examples/user_service/CMakeFiles/myrpc_user_proto.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmyrpc_user_proto.a"
-)
--- build_integration/examples/user_service/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/caigq/codes/MyRpcV2/examples/user_service
-# Build directory: /home/caigq/codes/MyRpcV2/build_integration/examples/user_service
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
